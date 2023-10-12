fdfc06838386ef3ded5408c74bd13b4b4503729d
carlos23fb <jcflobe@gmail.com>
2023-10-12 04:56:54 -0700

    no frills react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Loba",
+                animal: "Perro",
+                breed: "Corriente"
+            }),
+            React.createElement(Pet, {
+                name: "Princesa",
+                animal: "Perro",
+                breed: "Hell Hound"
+            }),
+            React.createElement(Pet, {
+                name: "Arena",
+                animal: "Cat",
+                breed: "Corriente"
+            }),
+        ]
+    )
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App))
