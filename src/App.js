08c28573b0d88048cbdd2cc45582beac12b5e492
carlos23fb <jcflobe@gmail.com>
2023-10-13 04:32:31 -0700

    npm & Prettier setup

--- src/App.js
+++ src/App.js
@@ -3,34 +3,30 @@ const Pet = (props) => {
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
         React.createElement("h2", {}, props.breed),
-    ])
-}
-
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Loba",
-                animal: "Perro",
-                breed: "Corriente"
-            }),
-            React.createElement(Pet, {
-                name: "Princesa",
-                animal: "Perro",
-                breed: "Hell Hound"
-            }),
-            React.createElement(Pet, {
-                name: "Arena",
-                animal: "Cat",
-                breed: "Corriente"
-            }),
-        ]
-    )
+    ]);
 };
 
-const container = document.getElementById('root');
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Loba",
+            animal: "Perro",
+            breed: "Corriente",
+        }),
+        React.createElement(Pet, {
+            name: "Princesa",
+            animal: "Perro",
+            breed: "Hell Hound",
+        }),
+        React.createElement(Pet, {
+            name: "Arena",
+            animal: "Cat",
+            breed: "Corriente",
+        }),
+    ]);
+};
+
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App))
+root.render(React.createElement(App));
